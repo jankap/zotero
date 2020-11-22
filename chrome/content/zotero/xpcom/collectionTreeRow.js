b2a4886782c66e8a0024f7c3f7b0b4b2a044e5d3
jankap <jan.kappen@t-online.de>
2020-11-23 00:44:23 +0100

    Revert "Fix tag selector not showing child item tags in "Title, Creator, Year" mode"
    This reverts commit 2da6eb8f701339b3c5c0f61c3361839514189ba7.

--- chrome/content/zotero/xpcom/collectionTreeRow.js
+++ chrome/content/zotero/xpcom/collectionTreeRow.js
@@ -336,7 +336,6 @@ Zotero.CollectionTreeRow.prototype.getSearchObject = Zotero.Promise.coroutine(fu
 		// Library root
 		if (this.isLibrary(true)) {
 			s.addCondition('noChildren', 'true');
-			// Allow tag selector to match child items in "Title, Creator, Year" mode
 			includeScopeChildren = true;
 		}
 		else if (this.isCollection()) {
@@ -345,7 +344,6 @@ Zotero.CollectionTreeRow.prototype.getSearchObject = Zotero.Promise.coroutine(fu
 			if (Zotero.Prefs.get('recursiveCollections')) {
 				s.addCondition('recursive', 'true');
 			}
-			// Allow tag selector to match child items in "Title, Creator, Year" mode
 			includeScopeChildren = true;
 		}
 		else if (this.isPublications()) {
@@ -365,6 +363,10 @@ Zotero.CollectionTreeRow.prototype.getSearchObject = Zotero.Promise.coroutine(fu
 	
 	if (this.isTrash()) {
 		s2.addCondition('deleted', 'true');
+	}
+	// Don't search child items in "Title, Creator, Year" mode
+	if (Zotero.Prefs.get('search.quicksearch-mode') == 'titleCreatorYear') {
+		includeScopeChildren = false;
 	}
 	s2.setScope(s, includeScopeChildren);
 	

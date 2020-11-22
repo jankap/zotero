b3384347b972cffdbaa4d0dcec4d68f6f46b6c24
jankap <jan.kappen@t-online.de>
2020-11-23 00:44:23 +0100

    Revert "Include only related-item relations when duplicating items"
    This reverts commit 48a203cfcd4ec70bee062b9c28368751bd600bfb.

--- chrome/content/zotero/xpcom/data/item.js
+++ chrome/content/zotero/xpcom/data/item.js
@@ -4006,23 +4006,7 @@ Zotero.Item.prototype.clone = function (libraryID, options = {}) {
 	
 	if (sameLibrary) {
 		// DEBUG: this will add reverse-only relateds too
-		let relations = this.getRelations();
-		
-		// Only include certain relations
-		let predicates = [
-			Zotero.Relations.relatedItemPredicate,
-		];
-		let any = false;
-		let newRelations = {};
-		for (let predicate of predicates) {
-			if (relations[predicate]) {
-				newRelations[predicate] = relations[predicate];
-				any = true;
-			}
-		}
-		if (any) {
-			newItem.setRelations(newRelations);
-		}
+		newItem.setRelations(this.getRelations());
 	}
 	
 	return newItem;

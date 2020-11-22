e5e25503854d8700b2cbd30269231e0571285649
jankap <jan.kappen@t-online.de>
2020-11-23 00:44:23 +0100

    Revert "Apply subtitle capitalization to dependent styles based on parent"
    This reverts commit 2bfd6e72137f089f535ada6b4cb5b81004ab5cef.

--- chrome/content/zotero/xpcom/style.js
+++ chrome/content/zotero/xpcom/style.js
@@ -649,6 +649,12 @@ Zotero.Style = function (style, path) {
 	this.locale = Zotero.Utilities.xpathText(doc, '/csl:style/@default-locale',
 		Zotero.Styles.ns) || null;
 	
+	// APA and some similar styles capitalize the first word of subtitles
+	this._uppercaseSubtitles = false;
+	var uppercaseSubtitlesRE = /^apa($|-)|^(academy-of-management|freshwater-science)/;
+	var shortIDMatches = this.styleID.match(/\/?([^/]+)$/);
+	this._uppercaseSubtitles = !!shortIDMatches && uppercaseSubtitlesRE.test(shortIDMatches[1]);
+	
 	this._class = doc.documentElement.getAttribute("class");
 	this._usesAbbreviation = !!Zotero.Utilities.xpath(doc,
 		'//csl:text[(@variable="container-title" and @form="short") or (@variable="container-title-short")][1]',
@@ -692,11 +698,6 @@ Zotero.Style.prototype.getCiteProc = function(locale, automaticJournalAbbreviati
 		}
 	}
 	
-	// APA and some similar styles capitalize the first word of subtitles
-	var uppercaseSubtitlesRE = /^apa($|-)|^academy-of-management($|-)|^(freshwater-science)/;
-	var shortIDMatches = this.styleID.match(/\/?([^/]+)$/);
-	var uppercaseSubtitles = !!shortIDMatches && uppercaseSubtitlesRE.test(shortIDMatches[1]);
-	
 	// determine version of parent style
 	var overrideLocale = false; // to force dependent style locale
 	if(this.source) {
@@ -716,14 +717,7 @@ Zotero.Style.prototype.getCiteProc = function(locale, automaticJournalAbbreviati
 			overrideLocale = true;
 			locale = this.locale;
 		}
-		
-		// Turn on uppercase subtitles if parent style matches
-		if (!uppercaseSubtitles) {
-			let shortIDMatches = parentStyle.styleID.match(/\/?([^/]+)$/);
-			uppercaseSubtitles = !!shortIDMatches && uppercaseSubtitlesRE.test(shortIDMatches[1]);
-		}
-	}
-	else {
+	} else {
 		var version = this._version;
 	}
 	
@@ -760,7 +754,7 @@ Zotero.Style.prototype.getCiteProc = function(locale, automaticJournalAbbreviati
 		var citeproc = new Zotero.CiteProc.CSL.Engine(
 			new Zotero.Cite.System({
 				automaticJournalAbbreviations,
-				uppercaseSubtitles
+				uppercaseSubtitles: this._uppercaseSubtitles
 			}),
 			xml,
 			locale,

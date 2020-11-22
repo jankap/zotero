859b36e44e9c61be4480fcbf162ccd1750419da5
jankap <jan.kappen@t-online.de>
2020-11-23 00:44:23 +0100

    Revert "Fix invalid HTML in note from Quick Copy"
    This reverts commit def23abdd1e18885ff8cf0269a2c0a4252f126b1.

--- chrome/content/zotero/xpcom/quickCopy.js
+++ chrome/content/zotero/xpcom/quickCopy.js
@@ -278,15 +278,13 @@ Zotero.QuickCopy = new function() {
 			
 			// If all notes, export full content
 			if (allNotes) {
-								var content = [];
-				let parser = Components.classes["@mozilla.org/xmlextras/domparser;1"]
-					.createInstance(Components.interfaces.nsIDOMParser);
-				let docHTML = '<html><body><div class="zotero-notes"/></body></html>';
-				let doc = parser.parseFromString(docHTML, 'text/html');
-				let textDoc = parser.parseFromString(docHTML, 'text/html');
-				let container = doc.body.firstChild;
-				let textContainer = textDoc.body.firstChild;
-				
+				var content = [],
+					parser = Components.classes["@mozilla.org/xmlextras/domparser;1"]
+						.createInstance(Components.interfaces.nsIDOMParser),
+					doc = parser.parseFromString('<div class="zotero-notes"/>', 'text/html'),
+					textDoc = parser.parseFromString('<div class="zotero-notes"/>', 'text/html'),
+					container = doc.documentElement,
+					textContainer = textDoc.documentElement;
 				for (var i=0; i<notes.length; i++) {
 					var div = doc.createElement("div");
 					div.className = "zotero-note";
